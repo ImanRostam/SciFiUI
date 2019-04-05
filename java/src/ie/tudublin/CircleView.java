5779f4da183ff8dff9299ff78411240f0d17fec4
ImanRostam <c17367011@mydit.ie>
2019-04-05 16:23:53 +0100

    Reload Class - changed cicrcles

--- java/src/ie/tudublin/CircleView.java
+++ java/src/ie/tudublin/CircleView.java
@@ -1,29 +1,29 @@
 package ie.tudublin;
 
-import processing.core.PApplet;
-
-public class CircleView extends PApplet
+public class CircleView extends UI
 {
     public void settings()
     {
     fullScreen();
     }
 
-    public void render()
+    public void circle()
     {
     float x = width/2;
     float y = height/2;
+    float space = 10;
 
-        for(int i = 0; i < 5; i++) 
+        for(float i = 0; i < 5; i++) 
         {
             noFill();
             stroke(255, 0, 0);
-            ellipse(x,y, i*(30), i*(30));
+            ellipse(x,y, 500, 500);
+            x = x + space ;
         }
     }
 
     public void draw()
     {
-        render();
+        circle();
     }
 }
