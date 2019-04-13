7ef4129c56c3fe75c8c51fcf39015fb7e7532308
ImanRostam <c17367011@mydit.ie>
2019-04-13 14:50:07 +0100

    Made CPButton class and implemented it using the super class

---
+++ java/src/ie/tudublin/CPButton.java
@@ -0,0 +1,16 @@
+package ie.tudublin;
+
+import processing.core.PApplet;
+
+public class CPButton extends UIElement
+{
+    public CPButton(int x, int y, PApplet ui)
+    {
+        super(x, y, ui);
+    }
+
+    public void render()
+    {
+        
+    }
+}
