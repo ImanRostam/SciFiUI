7ca9408d5d4977fb6ae65fb469bde660d7cab048
ImanRostam <c17367011@mydit.ie>
2019-04-13 13:19:53 +0100

    ScrollHandle class: Added the Handle on top of the scroll bar

--- java/src/ie/tudublin/ScrollBar.java
+++ java/src/ie/tudublin/ScrollBar.java
@@ -11,8 +11,15 @@ public class ScrollBar extends UIElement
 
     public void render()
     {
-        ui.stroke(0);
+        // Big Bar
+        ui.stroke(127);
+        ui.strokeWeight(1);
         ui.fill(0);
         ui.rect(415, ui.height-100, 10, ui.height);
+
+        // Small Bar
+        ui.stroke(127);
+        ui.fill(127);
+        ui.rect(419.99f, ui.height-97, 1, ui.height);
     }
 }
--- java/src/ie/tudublin/ScrollHandle.java
+++ java/src/ie/tudublin/ScrollHandle.java
@@ -11,6 +11,8 @@ public class ScrollHandle extends UIElement
 
     public void render()
     {
-        
+        ui.stroke(0);
+        ui.fill(255, 0, 0);
+        ui.rect(405, ui.height-95, 30, 10);
     }
 }
