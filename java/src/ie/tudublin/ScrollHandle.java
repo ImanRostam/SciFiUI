815e6e2e26de4224d6dc6be6131d1931defea5c3
ImanRostam <c17367011@mydit.ie>
2019-04-13 13:07:58 +0100

    Made ScrollHandle class and implemented it using the super class

---
+++ java/src/ie/tudublin/ScrollHandle.java
@@ -0,0 +1,16 @@
+package ie.tudublin;
+
+import processing.core.PApplet;
+
+public class ScrollHandle extends UIElement
+{
+    public ScrollHandle(int x, int y, PApplet ui)
+    {
+        super(x, y, ui);
+    }
+
+    public void render()
+    {
+        
+    }
+}
